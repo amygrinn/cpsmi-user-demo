2b703eab9fe33dcf7f5f90f641fa6fd4a5e5d9d1
Tyler Grinn <tylergrinn@gmail.com>
2018-04-11 20:33:18 -0400

    switched to observables

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
-$(function() {
+$(() => {
+  Rx.Observable
+  .fromEvent($("#search"), 'keyup')
+  .map(() => $("#search").val()) // Get search string
+  .do(val => !val && $("#results").html("")) // If input empty, clear results list
+  .pipe(Rx.operators.debounceTime(500)) // Only request users at most every 500 ms
+  .pipe(Rx.operators.switchMap( // switchMap only emits values from most recent request made
+    search => Rx.Observable.fromPromise(
+      $.get("/api/users?" + $.param({ search })).promise() // Request users
+    ))
+  )
+  .filter(() => !!$("#search").val()) // Check to see if input cleared while requesting data
+  .map(data => JSON.parse(data))
+  .subscribe(data => { // Display results
+    let htmlStr = ""
 
+    if(data.length > 0) {
+      data.forEach( function(user) {
+        htmlStr += `<li>${user.name}, ${user.phone}, ${user.email}</li>`
+      })
+    } else {
+      htmlStr = "No results found"
+    }
 
-  $("#search_users_form").submit(function(event) {
-
-    event.preventDefault()
-
-    $.get(
-      "/api/users?" + $.param({ search: $("#search").val() }),
-      function(data) {
-
-        data = JSON.parse(data)
-
-        let htmlStr = ""
-
-        if(data.length > 0) {
-
-          data.forEach( function(user) {
-            htmlStr += `<li>${user.name}, ${user.phone}, ${user.email}</li>`
-          })
-
-        } else {
-          htmlStr = "No results found"
-        }
-
-        $("#results").html(htmlStr)
-      }
-    )
+    $("#results").html(htmlStr)
   })
 })
